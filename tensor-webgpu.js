a367f5316f7918646034bd05a7dbd0a2c9e57be1
xnought <drbertucci@hotmail.com>
2025-01-27 16:04:21 -0500

    random

--- tensor-webgpu.js
+++ tensor-webgpu.js
@@ -1,2 +1,101 @@
-import { GPU } from "./webgpu-compute";
-console.log(GPU);
+import { GPU, assert } from "./webgpu-compute";
+
+/** @typedef {"f32" | "f64" | "u32"} DType*/
+/** @typedef {number[]} Shape */
+/** @typedef {Float32ArrayConstructor | Float64ArrayConstructor | Uint32ArrayConstructor} TypedArray */
+
+/** @type {Record<DType, TypedArray>}*/
+const DTypedArray = {
+	f32: Float32Array,
+	f64: Float64Array,
+	u32: Uint32Array,
+};
+
+/**
+ * Gives total length (multiply shape across)
+ * @param {Shape} shape
+ */
+function length(shape) {
+	let prod = 1;
+	for (const s of shape) prod *= s;
+	return prod;
+}
+
+/**
+ * Computes how to enumerate the ndarray
+ * @param {Shape} shape
+ */
+function strides(shape) {
+	let strides = new Array(shape.length);
+	strides[strides.length - 1] = 1;
+
+	for (let i = shape.length - 1; i > 0; i--) {
+		console.log(shape[i]);
+	}
+	return strides;
+}
+
+export class Tensor {
+	/**
+	 * @param {GPU} gpu
+	 * @param {GPUBuffer} gpuBuffer
+	 * @param {Shape} shape
+	 * @param {DType} dtype
+	 */
+	constructor(gpu, gpuBuffer, shape, dtype) {
+		this.gpu = gpu;
+		this.gpuBuffer = gpuBuffer;
+		this.shape = shape;
+		this.strides = strides(shape);
+		this.dtype = dtype;
+		this.freed = false;
+	}
+
+	get DTypedArray() {
+		return DTypedArray[this.dtype];
+	}
+
+	/**
+	 * Random uniform from [0, 1)
+	 * @param {GPU} gpu
+	 * @param {Shape} shape
+	 * @param {DType} dtype
+	 * @returns {Tensor}
+	 */
+	static random(gpu, shape, dtype = "f32") {
+		const cpuRandom = new DTypedArray[dtype](length(shape))
+			.fill(0)
+			.map((_) => Math.random());
+		const gpuBuffer = gpu.memAlloc(cpuRandom.byteLength);
+		gpu.memcpyHostToDevice(gpuBuffer, cpuRandom);
+		return new Tensor(gpu, gpuBuffer, shape, dtype);
+	}
+
+	async print() {
+		this.assertNotFreed();
+
+		const cpuBuffer = await this.gpu.mapGPUToCPU(
+			this.gpuBuffer,
+			this.DTypedArray
+		);
+		console.log(cpuBuffer, this.dtype);
+	}
+
+	free() {
+		this.assertNotFreed(); // don't free twice
+
+		this.gpu.free(this.gpuBuffer);
+		this.freed = true;
+	}
+
+	assertNotFreed() {
+		assert(!this.freed, "This GPU Buffer has already been freed.");
+	}
+}
+
+export async function dev() {
+	const gpu = await GPU.init();
+	const a = Tensor.random(gpu, [4, 1], "f32");
+	await a.print();
+	a.free();
+}
